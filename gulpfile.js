336380560bdfb88d204b93ece8542ea19e5c63c5
Vazquez <josefina.g.vazquez@accenture.com>
2019-04-11 15:47:14 -0500

    creating schedule app

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp');
+var sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync');
+
+const scripts = require('./scripts');
+const styles = require('./styles');
+ 
+gulp.task('sass', function () {
+    return gulp.src(styles)
+      .pipe(sass.sync().on('error', sass.logError))
+      .pipe(gulp.dest('./dist/css'))
+      .pipe(browserSync.reload({
+          stream: true
+      }));
+  });
+gulp.task('js', function(){ 
+    gulp.src(scripts)
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('html', function(){
+    gulp.src('./app/**/*.html')
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
+});
+
+gulp.task('build', function(){
+    gulp.start(['js','html','sass']);
+});
+
+
+gulp.task('browser-sync', function(){
+    browserSync.init(null, {
+        open: false,
+        server: {
+            baseDir: 'dist'
+        }
+    })
+});
+
+
+gulp.task('start', function(){
+    devMode = true;
+    gulp.start(['build','browser-sync']);
+    gulp.watch(['./app/**/*.js'],['js']);
+    gulp.watch(['./app/**/*.html'],['html']);
+    gulp.watch(['./app/scss/**/*.scss'],['sass']);
+});
